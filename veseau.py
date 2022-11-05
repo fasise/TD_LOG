22d947466d529c27b1fc71e9cd446ebe9a32b233
fasise <114915933+fasise@users.noreply.github.com>
2022-11-05 04:10:35 +0100

    Update veseau.py

--- veseau.py
+++ veseau.py
@@ -56,11 +56,28 @@ class Airfact(Vaisseau):
         if z==1 :
             super().go_to(x,y,z)
             
-            
-            # espace
-            
-            
-            
+class Battlefield:
+    def _init_(self):
+        self.vessels = []
+
+    def add_vessel(self,vessel):
+        coor = vessel.get_coordinates()
+        max_hits = vessel.max_hits
+        for existing_vessel in self.vessels:
+            if existing_vessel.get_coordinates() == coor:
+                return False
+            max_hits += existing_vessel.max_hits
+        if max_hits > 22:
+            return False
+        self.vessels.append(vessel)
+
+    def receive(self,x,y,z):
+        coor = (x,y,z)
+        for existing_vessel in self.vessels:
+            if existing_vessel.get_coordinates() == coor:
+                return True
+        return False        
+           
     def recevoir(self,x,y,z):
         joueur= (x,y,z)
         for i in self.vaisseaux:
